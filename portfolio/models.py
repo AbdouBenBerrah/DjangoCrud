3f8be12d79582e9f20e10e8ec19774fc7dfd4cb9
AbdouBenberrah <abdou.benberrah@gmail.com>
2020-07-21 21:48:28 +0200

    Databases

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    photo = models.ImageField(upload_to='portfolio/images/')
+    image = models.ImageField(upload_to='portfolio/images/')
     link = models.URLField(blank=True)
